fee10e6b42b219d428722314d6874d1ede490361
MerceRares <72392521+MerceRares@users.noreply.github.com>
2025-05-22 16:04:38 +0300

    Add project files.

---
+++ Hangman_game/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Hangman_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblWordDisplay = new Label();
+            btnGuess = new Button();
+            tbGuess = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblWordDisplay
+            // 
+            lblWordDisplay.BorderStyle = BorderStyle.Fixed3D;
+            lblWordDisplay.Font = new Font("SansSerif", 28.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            lblWordDisplay.Location = new Point(26, 21);
+            lblWordDisplay.Name = "lblWordDisplay";
+            lblWordDisplay.RightToLeft = RightToLeft.No;
+            lblWordDisplay.Size = new Size(464, 67);
+            lblWordDisplay.TabIndex = 0;
+            lblWordDisplay.Text = "Spanzuratoarea";
+            lblWordDisplay.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnGuess
+            // 
+            btnGuess.BackColor = Color.Gold;
+            btnGuess.Font = new Font("SansSerif", 28.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            btnGuess.Location = new Point(12, 376);
+            btnGuess.Name = "btnGuess";
+            btnGuess.Size = new Size(248, 62);
+            btnGuess.TabIndex = 1;
+            btnGuess.Text = "Ghiceste";
+            btnGuess.UseVisualStyleBackColor = false;
+            btnGuess.Click += btnGuess_Click;
+            // 
+            // tbGuess
+            // 
+            tbGuess.Font = new Font("SansSerif", 28.1999989F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            tbGuess.Location = new Point(383, 376);
+            tbGuess.Name = "tbGuess";
+            tbGuess.Size = new Size(96, 62);
+            tbGuess.TabIndex = 2;
+            tbGuess.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Location = new Point(23, 99);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(467, 236);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Brown;
+            ClientSize = new Size(517, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(tbGuess);
+            Controls.Add(btnGuess);
+            Controls.Add(lblWordDisplay);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblWordDisplay;
+        private Button btnGuess;
+        private TextBox tbGuess;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Hangman_game/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Hangman_game
+{
+    public partial class Form1 : Form
+    {
+        List<string> words = new List<string>
+        {
+            "farfurie",
+            "televizor",
+            "monociclu",
+            "lasou",
+            "fotbal",
+            "pensionar",
+            "trabant",
+            "laminat",
+            "parchet",
+            "animal"
+        };
+        int incorrectGuess;
+        char[] displayWord;
+        string selectedWord;
+        Random random;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            incorrectGuess = 0;
+            random = new Random();
+            selectedWord = words[random.Next(words.Count)];
+            displayWord = new string('_', selectedWord.Length).ToCharArray();
+            lblWordDisplay.Text = string.Join(" ", displayWord);
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbGuess.Text))
+                return;
+
+            char guess = tbGuess.Text.ToLower()[0];
+            bool correctGuess = false;
+
+            for (int i = 0; i < selectedWord.Length; i++)
+            {
+                if (selectedWord[i] == guess)
+                {
+                    displayWord[i] = guess;
+                    correctGuess = true;
+                }
+            }
+
+            lblWordDisplay.Text = string.Join(" ", displayWord);
+
+            if (!correctGuess)
+            {
+                UpdateHangmanImage();
+            }
+
+            if (!lblWordDisplay.Text.Contains("_"))
+            {
+                MessageBox.Show("Felicitări, ai câștigat!");
+                Application.Exit();
+            }
+
+            tbGuess.Clear();
+            tbGuess.Focus();
+        }
+
+        private void UpdateHangmanImage()
+        {
+            incorrectGuess++;
+            switch (incorrectGuess)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources._7;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources._8;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources._9;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources._10;
+                    MessageBox.Show("Ai pierdut! Cuvântul era: " + selectedWord);
+                    Application.Restart();
+                    break;
+            }
+        }
+    }
+}
